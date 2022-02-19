6d4d4370cf53a4a945bd93e5d5b21ae8fc03bf13
Tristan Clark <tristansclark@gmail.com>
2022-02-18 21:39:52 -0500

    Updated random tetrominoes

--- tetris/app.js
+++ tetris/app.js
@@ -5,41 +5,69 @@ document.addEventListener('DOMContentLoaded', () => {
   const StartBtn = document.querySelector('#start-button');
   const width = 10;
 
-  //The Tetrominoes
+  // The Tetrominoes
   const lTetromino = [
     [1, width+1, width*2+1, 2],
     [width, width+1, width+2, width*2+2],
     [1, width+1, width*2+1, width*2],
     [width, width*2, width*2+1, width*2+2]
-  ]
+  ];
 
   const zTetromino = [
     [0,width,width+1,width*2+1],
     [width+1, width+2,width*2,width*2+1],
     [0,width,width+1,width*2+1],
     [width+1, width+2,width*2,width*2+1]
-  ]
+  ];
 
   const tTetromino = [
     [1,width,width+1,width+2],
     [1,width+1,width+2,width*2+1],
     [width,width+1,width+2,width*2+1],
     [1,width,width+1,width*2+1]
-  ]
+  ];
 
   const oTetromino = [
     [0,1,width,width+1],
     [0,1,width,width+1],
     [0,1,width,width+1],
     [0,1,width,width+1]
-  ]
+  ];
 
   const iTetromino = [
     [1,width+1,width*2+1,width*3+1],
     [width,width+1,width+2,width+3],
     [1,width+1,width*2+1,width*3+1],
     [width,width+1,width+2,width+3]
-  ]
+  ];
 
-  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+  let currentPosition = 4;
+  let currentRotation = 0;
+
+
+  console.log(theTetrominoes[0][0]);
+
+  // randomly select a Tetromino and its first rotation
+  let random = Math.floor(Math.random()*theTetrominoes.length);
+  let current = theTetrominoes[random][currentRotation];
+
+  // draw the Tetromino
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino');
+      //squares[currentPosition + index].style.backgroundColor = colors[random];
+    });
+  }
+
+  // undraw the Tetromino
+  function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino');
+      //squares[currentPosition + index].style.backgroundColor = ''
+    })
+  }
+
+  draw()
 });
