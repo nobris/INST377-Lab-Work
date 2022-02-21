94303308048011eb1a21bf2d95253d1b1ecbf9d5
Tristan Clark <tristansclark@gmail.com>
2022-02-21 02:59:15 -0500

    Completed Lab 4

---
+++ client/lab_4/script.js
@@ -0,0 +1,42 @@
+let slidePosition = 0;
+const slides = document.querySelectorAll('.photo-grid-item');
+const totalSlides = slides.length;
+
+document.
+  querySelector('.carousel-actions-next')
+  .addEventListener("click", function() {
+    moveToNextSlide();
+  });
+
+document.
+  querySelector('.carousel-actions-prev')
+  .addEventListener("click", function() {
+    moveToPrevSlide();
+  });
+
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove('carousel-item-visible');
+    slide.classList.add('carousel-item-hidden')
+  }
+
+  slides[slidePosition].classList.add('carousel-item-visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+  updateSlidePosition();
+}
